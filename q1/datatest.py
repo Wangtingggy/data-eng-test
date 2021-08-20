23c9e03a8793512fb720773652d9fc9771d86e72
tohwangting <wangtingggy@gmail.com>
2021-08-21 01:41:20 +0800

    included comments in code for processing data in airflow

--- q1/datatest.py
+++ q1/datatest.py
@@ -24,12 +24,15 @@ dag = DAG('datafile', default_args=default_args)
 
 def task_read13():
     print("Reading dataset1 and dataset2")
+    # Read dataset
     dataset1 = pd.read_csv('/usr/local/airflow/dags/dataset1.csv')
     dataset2 = pd.read_csv('/usr/local/airflow/dags/dataset2.csv')
     
+    # Removed rows with names equals null
     dataset1 = dataset1[dataset1['name'].notnull()]
     dataset2 = dataset2[dataset2['name'].notnull()]
     
+    # Created first name and last name columns
     name_split_ds1 = dataset1['name'].str.split(n=1, expand=True)
     name_split_ds1 = name_split_ds1.rename(columns = {0: 'first_name', 1: 'last_name'})
     dataset1 = dataset1.join(name_split_ds1)
@@ -40,13 +43,16 @@ def task_read13():
     dataset2 = dataset2.join(name_split_ds2)
     dataset2.drop('name', axis=1, inplace=True)
     
+    # check for float type
     if (dataset1['price'].dtypes != 'float64' or dataset2['price'].dtypes != 'float64'):
         dataset1['price'] = dataset1['price'].astype('float64')
         dataset2['price'] = dataset2['price'].astype('float64')
     
+    # check if price >= 100
     dataset1['above_100'] = dataset1['price'].apply(lambda x: True if x > 100 else False)
     dataset2['above_100'] = dataset2['price'].apply(lambda x: True if x > 100 else False)
     
+    # save processed dataframe
     date_time = datetime.now().strftime("%Y%m%dT%H%M%S")
     dataset1.to_csv(f"/usr/local/airflow/dags/processed_dataset1_{date_time}.csv", index=True)
     dataset2.to_csv(f"/usr/local/airflow/dags/processed_dataset2_{date_time}.csv", index=True)
